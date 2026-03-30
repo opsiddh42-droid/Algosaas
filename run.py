34daa140a44404b9036656a3961bae89f08ef16b
opsiddh42-droid <opsiddh42@gmail.com>
2026-03-31 04:25:04 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import uvicorn
+import os
 
 if __name__ == "__main__":
+    # Render ya AWS par PORT automatically set hota hai, local ke liye 8000 default rakha hai
+    port = int(os.environ.get("PORT", 8000))
+    
     # "app.main:app" ka matlab hai app folder ke andar main.py me jo 'app' variable hai, use chalao.
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)

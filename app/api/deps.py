9f8ae33056d503f2b44e35517e64bb3f3ad43702
opsiddh42-droid <opsiddh42@gmail.com>
2026-03-31 02:31:49 +0530

    Create deps.py

---
+++ app/api/deps.py
@@ -0,0 +1,31 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from app.core.config import settings
+from app.core.database import get_collection
+
+# Yeh FastAPI ko batayega ki token kahan se uthana hai
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{settings.API_V1_STR}/auth/login")
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        # Token ko decode karna
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+        
+    # Database se user fetch karna
+    users_col = get_collection("users")
+    user = await users_col.find_one({"id": user_id})
+    if user is None:
+        raise credentials_exception
+        
+    return user
